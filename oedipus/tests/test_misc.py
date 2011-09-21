39dcb1e761c4c4b168f6cef6a1b164a822b45471
Erik Rose <erik@mozilla.com>
2011-09-20 21:13:35 -0700

    Move results tests into their own file.
    Rename test_api.py, which now holds tests that don't fit any other category.

---
+++ oedipus/tests/test_misc.py
@@ -0,0 +1,48 @@
+"""Test the mapping from our API to Sphinx's.
+
+We mock out all Sphinx's APIs.
+
+"""
+import fudge
+from nose import SkipTest
+from nose.tools import eq_, assert_raises
+import sphinxapi  # Comes in sphinx source code tarball
+
+from oedipus import S, SearchError
+from oedipus.tests import no_results, Biscuit
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_initialization(sphinx_client):
+    """S-wide default modes should get set when the SphinxClient is made."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()  # Call other crap on it if you want; I don't care.
+                  .expects('SetMatchMode').with_args(sphinxapi.SPH_MATCH_EXTENDED2)
+                  .expects('SetRankingMode').with_args(sphinxapi.SPH_RANK_PROXIMITY_BM25)
+                  .expects('SetSortMode').with_args(sphinxapi.SPH_SORT_EXTENDED, '@weight DESC, @id ASC'))
+    S(Biscuit)._sphinx()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_count(sphinx_client):
+    """Test ``S.__len__`` and ``S.count``."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('RunQueries').returns(no_results))
+    s = S(Biscuit)
+    eq_(len(s), 0)
+    eq_(s.count(), 0)
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_connection_failure(sphinx_client):
+    """``SearchError`` should be raised on connection error."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('RunQueries').returns(None))
+    assert_raises(SearchError, S(Biscuit).raw)
+
+
+def test_defaults():
+    """Defaults from the metadata should be obeyed."""
+    raise SkipTest
--- oedipus/tests/test_results.py
+++ oedipus/tests/test_results.py
@@ -1,40 +1,11 @@
-"""Test the mapping from our API to Sphinx's.
-
-We mock out all Sphinx's APIs.
-
-"""
 from unittest import TestCase
 
 import fudge
-from nose import SkipTest
 from nose.tools import eq_, assert_raises
-import sphinxapi  # Comes in sphinx source code tarball
 
-from oedipus import S, SearchError
+from oedipus import S
 import oedipus.tests
-from oedipus.tests import no_results, Biscuit, BaseSphinxMeta, crc32
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_initialization(sphinx_client):
-    """S-wide default modes should get set when the SphinxClient is made."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()  # Call other crap on it if you want; I don't care.
-                  .expects('SetMatchMode').with_args(sphinxapi.SPH_MATCH_EXTENDED2)
-                  .expects('SetRankingMode').with_args(sphinxapi.SPH_RANK_PROXIMITY_BM25)
-                  .expects('SetSortMode').with_args(sphinxapi.SPH_SORT_EXTENDED, '@weight DESC, @id ASC'))
-    S(Biscuit)._sphinx()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_count(sphinx_client):
-    """Test ``S.__len__`` and ``S.count``."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('RunQueries').returns(no_results))
-    s = S(Biscuit)
-    eq_(len(s), 0)
-    eq_(s.count(), 0)
+from oedipus.tests import no_results, Biscuit
 
 
 class SphinxMockingTestCase(TestCase):
@@ -126,17 +97,3 @@ class ResultsTestCase(SphinxMockingTestCase):
         """An empty values() call should raise ``TypeError``."""
         s = S(Biscuit)
         assert_raises(TypeError, s.values)
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_connection_failure(sphinx_client):
-    """``SearchError`` should be raised on connection error."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('RunQueries').returns(None))
-    assert_raises(SearchError, S(Biscuit).raw)
-
-
-def test_defaults():
-    """Defaults from the metadata should be obeyed."""
-    raise SkipTest

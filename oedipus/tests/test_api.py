e0f0bcae8e370e11fbb9769292e0c8a0d0cb2c89
Erik Rose <erik@mozilla.com>
2011-09-20 20:59:58 -0700

    Move filter tests into their own file.

--- oedipus/tests/test_api.py
+++ oedipus/tests/test_api.py
@@ -10,7 +10,7 @@ from nose import SkipTest
 from nose.tools import eq_, assert_raises
 import sphinxapi  # Comes in sphinx source code tarball
 
-from oedipus import S, MIN_LONG, MAX_LONG, SearchError
+from oedipus import S, SearchError
 import oedipus.tests
 from oedipus.tests import no_results, Biscuit, BaseSphinxMeta, crc32
 
@@ -41,102 +41,6 @@ def test_initialization(sphinx_client):
 
 
 @fudge.patch('sphinxapi.SphinxClient')
-def test_no_query(sphinx_client):
-    """Evaluating without calling query() should run an empty query."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('AddQuery').with_args('', 'biscuit')
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_simple_query(sphinx_client):
-    """A lone call to query(any_=...) should pass through to Sphinx.
-
-    Control chars should be stripped. Other kwargs should be ignored.
-
-    """
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('AddQuery').with_args('gerbil', 'biscuit')
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).query('^$gerbil', ignored_kwarg='dummy').raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_single_filter(sphinx_client):
-    """A filter call should be translated into the right Sphinx API calls."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('SetFilter').with_args('a', [1], False)
-                  .expects('SetFilter').with_args('b', [2, 3], False)
-
-                  # These 2 lines must be ordered such because fudge assumes,
-                  # when you call one method twice, that order matters.
-                  .expects('SetFilterRange').with_args('c', 4, MAX_LONG, False)
-                  .expects('SetFilterRange').with_args('d', MIN_LONG, 5, False)
-
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).filter(a=1,  # Test auto-listification of ints for equality filters.
-                      b__in=[2, 3],
-                      c__gte=4,
-                      d__lte=5).raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_single_exclude(sphinx_client):
-    """Assert conditions invert correctly."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('SetFilter').with_args('b', [2, 3], True)
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).exclude(b__in=[2, 3]).raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_range_exclude(sphinx_client):
-    """Putting a gte and a lte exclusion on the same field in the same call should set a single filter range exclusion on the query.
-
-    Otherwise, there would be no way to say "Give me docs with X between 1 and
-    10."
-
-    """
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('SetFilterRange').with_args('a', 1, 10, True)
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).exclude(a__gte=1, a__lte=10).raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_chained_excludes(sphinx_client):
-    """Test combining excludes, and test remaining filter inversions."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('SetFilter').with_args('b', [2], True)
-                  .expects('SetFilterRange').with_args('c', 4, MAX_LONG, True)
-                  .expects('SetFilterRange').with_args('d', MIN_LONG, 5, True)
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).exclude(b=2).exclude(c__gte=4).exclude(d__lte=5).raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_range_filter(sphinx_client):
-    """Putting a gte and a lte exclusion on the same field in the same call should set a single filter range exclusion on the query.
-
-    Otherwise, there would be no way to say "Give me docs with X between 1 and
-    10."
-
-    """
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('SetFilterRange').with_args('a', 1, 10, False)
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).filter(a__gte=1, a__lte=10).raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
 def test_order_by_fields(sphinx_client):
     """Test ordering by only field values."""
     (sphinx_client.expects_call().returns_fake()
@@ -157,16 +61,6 @@ def test_order_by_rank_explicitly(sphinx_client):
                              'a ASC, @weight DESC, @id ASC')
                   .expects('RunQueries').returns(no_results))
     S(Biscuit).order_by('a', '-@rank').raw()
-
-
-@fudge.patch('sphinxapi.SphinxClient')
-def test_filter_string_mapping(sphinx_client):
-    """String values need to be mapped to ints for filtering."""
-    (sphinx_client.expects_call().returns_fake()
-                  .is_a_stub()
-                  .expects('SetFilter').with_args('a', [crc32('test')], False)
-                  .expects('RunQueries').returns(no_results))
-    S(Biscuit).filter(a='test').raw()
 
 
 @fudge.patch('sphinxapi.SphinxClient')
@@ -316,9 +210,6 @@ def test_connection_failure(sphinx_client):
     assert_raises(SearchError, S(Biscuit).raw)
 
 
-def test_chained_filters():
-    """Test several filter() calls ANDed together."""
-
-
 def test_defaults():
     """Defaults from the metadata should be obeyed."""
+    raise SkipTest
---
+++ oedipus/tests/test_filters.py
@@ -0,0 +1,118 @@
+"""Tests for queries, filters, and excludes"""
+
+import fudge
+from nose import SkipTest
+
+from oedipus import S, MIN_LONG, MAX_LONG
+from oedipus.tests import no_results, Biscuit, crc32
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_no_query(sphinx_client):
+    """Evaluating without calling query() should run an empty query."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('AddQuery').with_args('', 'biscuit')
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_simple_query(sphinx_client):
+    """A lone call to query(any_=...) should pass through to Sphinx.
+
+    Control chars should be stripped. Other kwargs should be ignored.
+
+    """
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('AddQuery').with_args('gerbil', 'biscuit')
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).query('^$gerbil', ignored_kwarg='dummy').raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_single_filter(sphinx_client):
+    """A filter call should be translated into the right Sphinx API calls."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('SetFilter').with_args('a', [1], False)
+                  .expects('SetFilter').with_args('b', [2, 3], False)
+
+                  # These 2 lines must be ordered such because fudge assumes,
+                  # when you call one method twice, that order matters.
+                  .expects('SetFilterRange').with_args('c', 4, MAX_LONG, False)
+                  .expects('SetFilterRange').with_args('d', MIN_LONG, 5, False)
+
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).filter(a=1,  # Test auto-listification of ints for equality filters.
+                      b__in=[2, 3],
+                      c__gte=4,
+                      d__lte=5).raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_single_exclude(sphinx_client):
+    """Assert conditions invert correctly."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('SetFilter').with_args('b', [2, 3], True)
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).exclude(b__in=[2, 3]).raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_range_exclude(sphinx_client):
+    """Putting a gte and a lte exclusion on the same field in the same call should set a single filter range exclusion on the query.
+
+    Otherwise, there would be no way to say "Give me docs with X between 1 and
+    10."
+
+    """
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('SetFilterRange').with_args('a', 1, 10, True)
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).exclude(a__gte=1, a__lte=10).raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_chained_excludes(sphinx_client):
+    """Test combining excludes, and test remaining filter inversions."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('SetFilter').with_args('b', [2], True)
+                  .expects('SetFilterRange').with_args('c', 4, MAX_LONG, True)
+                  .expects('SetFilterRange').with_args('d', MIN_LONG, 5, True)
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).exclude(b=2).exclude(c__gte=4).exclude(d__lte=5).raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_range_filter(sphinx_client):
+    """Putting a gte and a lte exclusion on the same field in the same call should set a single filter range exclusion on the query.
+
+    Otherwise, there would be no way to say "Give me docs with X between 1 and
+    10."
+
+    """
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('SetFilterRange').with_args('a', 1, 10, False)
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).filter(a__gte=1, a__lte=10).raw()
+
+
+@fudge.patch('sphinxapi.SphinxClient')
+def test_filter_string_mapping(sphinx_client):
+    """String values need to be mapped to ints for filtering."""
+    (sphinx_client.expects_call().returns_fake()
+                  .is_a_stub()
+                  .expects('SetFilter').with_args('a', [crc32('test')], False)
+                  .expects('RunQueries').returns(no_results))
+    S(Biscuit).filter(a='test').raw()
+
+
+def test_chained_filters():
+    """Test several filter() calls ANDed together."""
+    raise SkipTest
